c34e2f80847cda99f156a670e8f5da923a619bba
aliefabdillah <aliefmabdillah09@gmail.com>
2022-03-26 09:43:57 +0700

    crate unit test

--- DiceRoller/app/src/test/java/com/example/diceroller/ExampleUnitTest.kt
+++ DiceRoller/app/src/test/java/com/example/diceroller/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.example.diceroller
 
+import org.junit.Assert.assertTrue
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -11,7 +10,9 @@ import org.junit.Assert.*
  */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun generates_number(){
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        assertTrue("The value of rollResult was not between 1 and 6", diceRoll in 1..6)
     }
 }
