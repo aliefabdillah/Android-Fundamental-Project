63698a1a70d87ea9d776343b757af3fff96f5e0d
aliefabdillah <aliefmabdillah09@gmail.com>
2022-03-26 09:35:08 +0700

    adding image to dice (succed)

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -19,28 +19,35 @@ class MainActivity : AppCompatActivity() {
 
 //        the method when button clicked
         rollButton.setOnClickListener {
-            rollDice(R.id.textView)
-            rollDice(R.id.textView2)
+            rollDice(R.id.imageView)
+            rollDice(R.id.imageView2)
             val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
             toast.show()
         }
+
+        // Do a dice roll when the app starts
+        rollDice(R.id.imageView)
+        rollDice(R.id.imageView2)
     }
 
-//    private fun rollDice2() {
-//        val dice2= Dice(6)
-//        val diceRoll2 = dice2.roll()
-//        val resultTextView: TextView = findViewById(R.id.textView)
-//        resultTextView.text = diceRoll2.toString()
-//    }
-
-    private fun rollDice(textView: Int) {
+    private fun rollDice(imageView: Int) {
 //        create new dice object with 6 side and use roll method from Dice class
         val dice = Dice(6)
         val diceRoll = dice.roll()
 
-//      update the textView with new number from dice
-        val resultTextView: TextView = findViewById(textView)
-        resultTextView.text = diceRoll.toString()
+//      update the imageView with new number from dice
+        val diceImage: ImageView = findViewById(imageView)
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = diceRoll.toString()
     }
 }
 
