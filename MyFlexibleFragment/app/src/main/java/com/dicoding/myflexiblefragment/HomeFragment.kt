1246fd955b664871784fe1b6bccb63c3c76abe76
aliefabdillah <aliefmabdillah09@gmail.com>
2022-08-25 11:12:38 +0700

    Membuat Fragment di activity

--- MyFlexibleFragment/app/src/main/java/com/dicoding/myflexiblefragment/HomeFragment.kt
+++ MyFlexibleFragment/app/src/main/java/com/dicoding/myflexiblefragment/HomeFragment.kt
@@ -1,59 +1,41 @@
 package com.dicoding.myflexiblefragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class HomeFragment : Fragment(), View.OnClickListener {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    /*
+    * metode onCreateView() berfungsi unuk layout interface didefinisikan dan ditransformasikan dari layout
+    * berupa file xml ke dalam objek view dengan menggunakan metode inflate().*/
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+        /*
+        * fungsi inflate merupakan objek dari LayoutInflater yang berfungsi untuk mengubah layout xml ke dalam bentuk objek viewgroup
+        * atau widget. fungsi inflate di sini yaitu untuk menampilkan layout dari Fragment, di mana layout yang ditampilkan
+        * di sini yaitu fragment_home.*/
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    /*
+    * Methode ini digunakan untuk menginisialiasi view dan mengatur action dari view*/
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val btnCategory: Button = view.findViewById(R.id.btn_category)      //pemanggilan findviewbyid harus menggunakan variabel view
+        /*
+        * Kode view.findViewById tersebut menandakan btn_category berada pada objek view di mana objek view
+        * berasal dari konversi fragment_home.xml. Bila hanya findViewById(R.id.btn_category), maka btn_category
+        * berada pada root layout, activity_main.xml.*/
+
+        btnCategory.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+
     }
 }
--- MyFlexibleFragment/app/src/main/java/com/dicoding/myflexiblefragment/MainActivity.kt
+++ MyFlexibleFragment/app/src/main/java/com/dicoding/myflexiblefragment/MainActivity.kt
@@ -2,10 +2,34 @@ package com.dicoding.myflexiblefragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //penggunaan instance dari fragment manager yang merupakan UI untuk mengorganisir objek fragment pada sebuah acitivty
+        val mFragmentManager = supportFragmentManager
+        val mHomeFragment = HomeFragment()      //instansiasi objek homefragment
+        val fragment = mFragmentManager.findFragmentByTag(HomeFragment::class.java.simpleName)
+
+        if (fragment !is HomeFragment) {
+            Log.d("MyFlexibleFragment", "Fragment Name :" + HomeFragment::class.java.simpleName)
+            mFragmentManager
+                //untuk memulai proses perubahan
+                .beginTransaction()
+
+                //menembahkan objek fragment ke dalam layout container
+                /*
+                *ayout container ini merupakan objek framelayout dengan ID frame_container. Ia memiliki
+                * tag dengan nama kelas dari HomeFragment itu sendiri. */
+                .add(R.id.frame_container, mHomeFragment, HomeFragment::class.java.simpleName)
+
+                // untuk mengeksekusi pemasangan fragment untuk ditampilkan ke antarmuka pengguna
+                .commit()
+        }
     }
+
+
 }
