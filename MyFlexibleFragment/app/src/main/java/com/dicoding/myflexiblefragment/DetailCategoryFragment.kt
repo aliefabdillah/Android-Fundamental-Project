5bfa4ddb71532f397b7c5383eaeaf7619d6a7b89
aliefabdillah <aliefmabdillah09@gmail.com>
2022-08-25 12:41:11 +0700

    Berpindah dari fragment ke Activity

--- MyFlexibleFragment/app/src/main/java/com/dicoding/myflexiblefragment/DetailCategoryFragment.kt
+++ MyFlexibleFragment/app/src/main/java/com/dicoding/myflexiblefragment/DetailCategoryFragment.kt
@@ -1,5 +1,6 @@
 package com.dicoding.myflexiblefragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -37,6 +38,7 @@ class DetailCategoryFragment : Fragment(){
         btnProfile = view.findViewById(R.id.btn_profile)
         btnShowDialog = view.findViewById(R.id.btn_show_dialog)
 
+        //tombol menampilkan dialog form
         btnShowDialog.setOnClickListener{
             val mOptionDialogFragment = OptionDialogFragment()
 
@@ -47,6 +49,17 @@ class DetailCategoryFragment : Fragment(){
             * yaitu DetailCategoryFragment*/
             val mFragmentManager = childFragmentManager
             mOptionDialogFragment.show(mFragmentManager, OptionDialogFragment::class.java.simpleName)
+        }
+
+        //tombol untuk beralih ke activity profile
+        btnProfile.setOnClickListener {
+            //caranya mirip seperti berpindah activity biasa
+            /*
+            * perbedaannya pada fragment harus menggunakan requireActivity untuk parameter
+            * context nya karena fungsi this hanya bisa dipanggil melalui activity,
+            * sedangkan untuk class nya masih sama*/
+            val mIntentProfile = Intent(requireActivity(), ProfileActivity::class.java)
+            startActivity(mIntentProfile)
         }
 
         if (savedInstanceState != null){
