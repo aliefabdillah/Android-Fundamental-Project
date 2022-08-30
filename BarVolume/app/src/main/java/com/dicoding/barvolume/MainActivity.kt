533b71bd3b0cea9b8076a6ebbaa8b8ccf2bcccd4
aliefabdillah <aliefmabdillah09@gmail.com>
2022-08-30 08:49:18 +0700

    Mengubah penggunaan findViewById menjadi menggunakan ViewBinding

--- BarVolume/app/src/main/java/com/dicoding/barvolume/MainActivity.kt
+++ BarVolume/app/src/main/java/com/dicoding/barvolume/MainActivity.kt
@@ -7,14 +7,10 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import com.dicoding.barvolume.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     //inisialisasi variabel untuk menampung data masukan
-    private lateinit var edtWidht: EditText
-    private lateinit var edtLength: EditText
-    private lateinit var edtHeight: EditText
-    private lateinit var resultView: TextView
-    private lateinit var btnCalculate: Button
 
     //save instance
     companion object {
@@ -22,10 +18,13 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         private const val STATE_RESULT = "state_result"
     }
 
+    //variable untuk view binding
+    private lateinit var binding: ActivityMainBinding
+
     //fungsi bawaan save instance state
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putString(STATE_RESULT, resultView.text.toString())    //menyimpan nilai result view ke bundle di state result
+        outState.putString(STATE_RESULT, binding.resultView.text.toString())    //menyimpan nilai result view ke bundle di state result
         /*
         * menyimpan kode ini menggunakan konsep key-value, dengan STATE_RESULT merupakai key dan valuenya adalah resultView
         * sebagai string
@@ -38,22 +37,18 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)       //inisilalisasi binding view
+        setContentView(binding.root)                                //set content binding ke root
 
-        edtWidht = findViewById(R.id.edit_width)    //mengambil id dari input lebar
-        edtHeight = findViewById(R.id.edit_height)  //mengambil id dari input tinggi
-        edtLength = findViewById(R.id.edit_length)  //mengambil id dari input panjang
-        btnCalculate = findViewById(R.id.btn_calculate) //mengambil id dari tombol result
-        resultView = findViewById(R.id.result_view)     //mengambil id dari text view hasil
-
-        btnCalculate.setOnClickListener(this)           //menambahkan fungsi setOnClick pada button
+        //maka untuk mendapatkan view id tinggal menambahkan kode binding di setiap element view
+        binding.btnCalculate.setOnClickListener(this)           //menambahkan fungsi setOnClick pada button
 
         //jika variabel save instance tidak kosong
         if (savedInstanceState != null){
             //mengambil value result perhitungan dari key state result
             val result = savedInstanceState.getString(STATE_RESULT)
             //dan menampilkan kembali di text view hasil
-            resultView.text = result
+            binding.resultView.text = result
         }
     }
 
@@ -61,9 +56,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         //jika parameter adalah button id
         if (v?.id == R.id.btn_calculate){
             //mengambil data string dari input
-            val inputL = edtLength.text.toString().trim()
-            val inputW = edtWidht.text.toString().trim()
-            val inputH = edtHeight.text.toString().trim()
+            val inputL = binding.editLength.text.toString().trim()
+            val inputW = binding.editWidth.text.toString().trim()
+            val inputH = binding.editHeight.text.toString().trim()
 
             //penanda apakah field input kosong atau tidak
             var isEmptyFields = false
@@ -71,25 +66,26 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             //jika field input panjang kosong
             if (inputL.isEmpty()){
                 isEmptyFields = true
-                edtLength.error = "Field ini tidak boleh kosong"
+                binding.editLength.error = "Field ini tidak boleh kosong"
             }
             //jika field input lebar kosong
             if (inputW.isEmpty()){
                 isEmptyFields = true
-                edtWidht.error = "Field ini tidak boleh kosong"
+                binding.editWidth.error = "Field ini tidak boleh kosong"
             }
             //jika field input tinggi kosong
             if (inputH.isEmpty()){
                 isEmptyFields = true
-                edtHeight.error = "Field ini tidak boleh kosong"
+                binding.editHeight.error = "Field ini tidak boleh kosong"
             }
+
 
             //jika tida ada field yang kosong
             if (!isEmptyFields)
             {
                 //algoritma untuk menghitung volume
                 val volume = inputL.toDouble() * inputW.toDouble() * inputH.toDouble()
-                resultView.text = volume.toString()     //mengubah nilai result menjadi nilai vulume yang dihasilkan
+                binding.resultView.text = volume.toString()     //mengubah nilai result menjadi nilai vulume yang dihasilkan
             }
         }
     }
