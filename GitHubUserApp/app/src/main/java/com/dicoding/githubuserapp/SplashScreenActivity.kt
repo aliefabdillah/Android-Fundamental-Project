3923a96d5ee8019ddaff3ebdbc3a50ed55384780
aliefabdillah <aliefmabdillah09@gmail.com>
2022-08-30 22:36:43 +0700

    First Commit GitHub User App - Create Splash Screen

---
+++ GitHubUserApp/app/src/main/java/com/dicoding/githubuserapp/SplashScreenActivity.kt
@@ -0,0 +1,26 @@
+package com.dicoding.githubuserapp
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.githubuserapp.databinding.ActivitySplashScreenBinding
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreenActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.githubLogo.alpha = 0f
+        binding.githubLogo.animate().setDuration(2000).alpha(1f).withEndAction {
+            val i = Intent(this@SplashScreenActivity, MainActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+    }
+}
