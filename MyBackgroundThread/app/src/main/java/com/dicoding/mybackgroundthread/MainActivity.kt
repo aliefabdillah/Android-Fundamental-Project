e14202fb42243c31149442c015cd6d49edabe5f4
aliefabdillah <aliefmabdillah09@gmail.com>
2022-09-08 09:48:01 +0700

    Change method using coroutines to handler background process

--- MyBackgroundThread/app/src/main/java/com/dicoding/mybackgroundthread/MainActivity.kt
+++ MyBackgroundThread/app/src/main/java/com/dicoding/mybackgroundthread/MainActivity.kt
@@ -6,6 +6,11 @@ import android.os.Handler
 import android.os.Looper
 import android.widget.Button
 import android.widget.TextView
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
@@ -24,29 +29,51 @@ class MainActivity : AppCompatActivity() {
         * perlu menggunakan kode Executor untuk membuat thread baru di belakang main thread saat ini
         * dan handler untuk update hasil dari proses ke komponen UI*/
 
-        val executor = Executors.newSingleThreadExecutor()      //executor untuk mengataasi background proses
-        val handler = Handler(Looper.getMainLooper())           //variabel handler
+        val executor = Executors.newSingleThreadExecutor()      //membuat hanya satu thread lain untuk menjalankan proses
+        val handler = Handler(Looper.getMainLooper())           //variabel handler untuk menjalankan proses di dalam handler pada main thread
 
+        //Kode dengan Executor dan handler
+//        btnStart.setOnClickListener {
+//            //kode executor untuk menjalan kan kode pada thread baru
+//            executor.execute {
+//                try {
+//                    //simulate process compressing
+//                    for (i in 0..10){
+//                        Thread.sleep(500)
+//                        val percentage = i*10
+//
+//                        //handler untuk update ui di main thread
+//                        handler.post {
+//                            if (percentage == 100){
+//                                tvStatus.setText(R.string.task_completed)
+//                            }else{
+//                                tvStatus.text = String.format(getString(R.string.compressing), percentage)
+//                            }
+//                        }
+//                    }
+//                }catch (e: InterruptedException) {
+//                    e.printStackTrace()
+//                }
+//            }
+//        }
+
+        //kode dengan coroutines
         btnStart.setOnClickListener {
-            //kode executor untuk menjalan kan kode pada thread baru
-            executor.execute {
-                try {
-                    //simulate process compressing
-                    for (i in 0..10){
-                        Thread.sleep(500)
-                        val percentage = i*10
+            lifecycleScope.launch(Dispatchers.Default){
+                //simulate process in background thread
+                for (i in 0..10){
+                    delay(500)
+                    val percentage = i * 10
 
-                        //handler untuk update ui di main thread
-                        handler.post {
-                            if (percentage == 100){
-                                tvStatus.setText(R.string.task_completed)
-                            }else{
-                                tvStatus.text = String.format(getString(R.string.compressing), percentage)
-                            }
+                    //berpindah thread ke main thread
+                    withContext(Dispatchers.Main){
+                        //update ui in main thread
+                        if (percentage == 100){
+                            tvStatus.setText(R.string.task_completed)
+                        }else{
+                            tvStatus.text = String.format(getString(R.string.compressing), percentage)
                         }
                     }
-                }catch (e: InterruptedException) {
-                    e.printStackTrace()
                 }
             }
         }
