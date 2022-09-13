3fab6a449e1b422f81999871c0c86c0d8c0af816
aliefabdillah <aliefmabdillah09@gmail.com>
2022-09-13 11:48:29 +0700

    Menambahakan Handler data menggunakan fitur LiveData ketika terjadi configurasi pada aplikasi

--- RestoranReview/app/src/main/java/com/dicoding/restoranreview/MainActivity.kt
+++ RestoranReview/app/src/main/java/com/dicoding/restoranreview/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
@@ -17,61 +18,55 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    companion object {
-        private const val TAG = "MainActivity"
-        private const val RESTAURANT_ID = "uewq1zg2zlskfw1e867"
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         supportActionBar?.hide()
+
+        //configurasi layout dari recycler view dan item decoration
         val layoutManager = LinearLayoutManager(this)
         binding.rvReview.layoutManager = layoutManager
         val itemDecoration = DividerItemDecoration(this, layoutManager.orientation)
         binding.rvReview.addItemDecoration(itemDecoration)
 
-        findRestaurant()
+        //connect view model to activity
+        val mainViewModel = ViewModelProvider(this@MainActivity)[MainViewModel::class.java]
 
+        //komponen observer data restaurant
+        mainViewModel.restaurant.observe(
+            this@MainActivity,
+        ) { restaurant ->
+            setRestaurantData(restaurant)
+        }
+
+        //komponen observer list customer review
+        mainViewModel.listReview.observe(
+            this@MainActivity,
+        ){ customerReview ->
+            /*
+            * Variaebl customer review akan selalu di update secara realtime oleh live data
+            * karena sudah menjadi bagian dari observer*/
+            setReviewData(customerReview)
+        }
+
+        //komponen observer loading
+        mainViewModel.isLoading.observe(
+            this@MainActivity,
+        ){
+            showLoading(it)
+        }
+
+        //handler button ketika di clikc
         binding.btnSend.setOnClickListener { view ->
-            postReview(binding.edReview.text.toString())
+            /*
+            * menjalankan method postReview dari backgroudn thread
+            * dengan parameter review input dari user*/
+            mainViewModel.postReview(binding.edReview.text.toString())
             val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
             imm.hideSoftInputFromWindow(view.windowToken, 0)
         }
-    }
-
-    private fun findRestaurant() {
-        showLoading(true)
-        val client = ApiConfig.getApiService().getRestaurant(RESTAURANT_ID)     //memanggil retrofit
-
-        /* enqueue berfungsi untuk menjalakan request asynchronus di background agar app tidak freezing/lag */
-        client.enqueue(object: retrofit2.Callback<RestaurantResponse> {
-            //jika callbak memiliki response
-            override fun onResponse(
-                call: Call<RestaurantResponse>,
-                response: Response<RestaurantResponse>
-            ) {
-                showLoading(false)
-                //cek apakah server mengembalikan kode 200 (OK)
-                if (response.isSuccessful) {
-                    val responseBody = response.body()                  //mengambil data dari body API
-                    if (responseBody != null) {
-                        setRestaurantData(responseBody.restaurant)              //set data restaurant ke view restaurant
-                        setReviewData(responseBody.restaurant.customerReviews)  //set data review ke view review
-                    }
-                } else {
-                    Log.e(TAG, "onFailure: ${response.message()}")
-                }
-            }
-
-            //ketika callbak failure
-            override fun onFailure(call: Call<RestaurantResponse>, t: Throwable) {
-                showLoading(false)
-                Log.e(TAG, "onFailure: ${t.message}")
-            }
-        })
     }
 
     //menyimpan data restaurant ke view untuk data restaurant
@@ -85,14 +80,8 @@ class MainActivity : AppCompatActivity() {
 
 //    menyimpan data review ke dalam view untuk listReview
     private fun setReviewData(consumerReviews: List<CustomerReviewsItem>) {
-        val listReview = ArrayList<String>()
-        for (review in consumerReviews) {
-            listReview.add(
-                """
-                ${review.review}
-                - ${review.name}
-                """.trimIndent()
-            )
+        val listReview = consumerReviews.map {
+            "${it.review}\n- ${it.name}"
         }
         val adapter = ReviewAdapter(listReview)
         binding.rvReview.adapter = adapter
@@ -106,31 +95,5 @@ class MainActivity : AppCompatActivity() {
         } else {
             binding.progressBar.visibility = View.GONE
         }
-    }
-
-    private fun postReview(review: String){
-        showLoading(true)
-        val client = ApiConfig.getApiService().postReview(RESTAURANT_ID, "Dicoding", review)
-
-        client.enqueue(object : retrofit2.Callback<PostReviewResponse>{
-            override fun onResponse(
-                call: Call<PostReviewResponse>,
-                response: Response<PostReviewResponse>
-            ) {
-                showLoading(false)
-                val responseBody = response.body()
-                if (response.isSuccessful && responseBody != null){
-                    setReviewData(responseBody.customerReviews)
-                }else{
-                    Log.e(TAG, "onFailuer : ${response.message()}")
-                }
-            }
-
-            override fun onFailure(call: Call<PostReviewResponse>, t: Throwable) {
-                showLoading(false)
-                Log.e(TAG, "onFailure: ${t.message}")
-            }
-
-        })
     }
 }
---
+++ RestoranReview/app/src/main/java/com/dicoding/restoranreview/MainViewModel.kt
@@ -0,0 +1,93 @@
+package com.dicoding.restoranreview
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import retrofit2.Call
+import retrofit2.Response
+
+class MainViewModel : ViewModel() {
+
+    /*
+    * BACKING PROPERTY
+    *
+    *  dengan membuat data yang bertipe MutableLiveData menjadi private (_listReview) dan yang
+    * bertipe LiveData menjadi public (listReview). Cara ini disebut dengan backing property.
+    * Dengan begitu Anda dapat mencegah variabel yang bertipe MutableLiveData diubah dari luar class.
+    * Karena memang seharusnya hanya ViewModel-lah yang dapat mengubah data.*/
+    private val _restaturant = MutableLiveData<Restaurant>()
+    val restaurant: LiveData<Restaurant> = _restaturant
+
+    private val _listReview = MutableLiveData<List<CustomerReviewsItem>>()
+    val listReview: LiveData<List<CustomerReviewsItem>> = _listReview
+
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    companion object {
+        private const val TAG = "MainViewModel"
+        private const val RESTAURANT_ID = "uewq1zg2zlskfw1e867"
+    }
+
+    init {
+        findRestaurant()
+    }
+
+    private fun findRestaurant() {
+        _isLoading.value = true
+        val client = ApiConfig.getApiService().getRestaurant(RESTAURANT_ID)     //memanggil retrofit
+
+        /* enqueue berfungsi untuk menjalakan request asynchronus di background agar app tidak freezing/lag */
+        client.enqueue(object: retrofit2.Callback<RestaurantResponse> {
+            //jika callbak memiliki response
+            override fun onResponse(
+                call: Call<RestaurantResponse>,
+                response: Response<RestaurantResponse>
+            ) {
+                _isLoading.value = false
+                //cek apakah server mengembalikan kode 200 (OK)
+                if (response.isSuccessful) {
+//                    val responseBody = response.body()                  //mengambil data dari body API
+                    _restaturant.value = response.body()?.restaurant                    //menyimpan nilai restaurant dari API ke variabel livedata
+                    _listReview.value = response.body()?.restaurant?.customerReviews    // menyimpan nilai review dari API ke variabel livedata
+//                    if (responseBody != null) {
+//                    }
+                } else {
+                    Log.e(TAG, "onFailure: ${response.message()}")
+                }
+            }
+
+            //ketika callbak failure
+            override fun onFailure(call: Call<RestaurantResponse>, t: Throwable) {
+                _isLoading.value = false
+                Log.e(TAG, "onFailure: ${t.message}")
+            }
+        })
+    }
+
+    fun postReview(review: String){
+        _isLoading.value = true
+        val client = ApiConfig.getApiService().postReview(RESTAURANT_ID, "Dicoding", review)
+
+        client.enqueue(object : retrofit2.Callback<PostReviewResponse>{
+            override fun onResponse(
+                call: Call<PostReviewResponse>,
+                response: Response<PostReviewResponse>
+            ) {
+                _isLoading.value = false
+                if (response.isSuccessful){
+                    _listReview.value = response.body()?.customerReviews        //menyimpan nilai customer review dari API ke variabel live data
+                }else{
+                    Log.e(TAG, "onFailure : ${response.message()}")
+                }
+            }
+
+            override fun onFailure(call: Call<PostReviewResponse>, t: Throwable) {
+                _isLoading.value = false
+                Log.e(TAG, "onFailure: ${t.message}")
+            }
+
+        })
+    }
+}
