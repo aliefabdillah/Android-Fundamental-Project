77922f7498462f223a61605b93cf15e9a3b0cb01
aliefabdillah <aliefmabdillah09@gmail.com>
2022-09-26 11:08:44 +0700

    Commit - Menambahkan Fitur PendingIntent untuk action sebuah Notifikasi

--- SimpleNotif/app/src/main/java/com/dicoding/simplenotif/MainActivity.kt
+++ SimpleNotif/app/src/main/java/com/dicoding/simplenotif/MainActivity.kt
@@ -2,8 +2,11 @@ package com.dicoding.simplenotif
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -26,12 +29,23 @@ class MainActivity : AppCompatActivity() {
     *Kelebihan dari cara ini yakni kita tidak perlu menginisialisasi Button tersebut di onCreate,
     * sehingga pemanggilan kode bisa lebih cepat dengan syarat memiliki paramter view pada method. */
     fun sendNotification(view: View){
+        //menambahkan pending Intent ke sebuah URL, selain ke sebuah URL dapat juga menambahkan intent untuk memanggil activity
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://dicoding.com"))
+        val pendingIntent = PendingIntent.getActivity(
+            this, 0, intent,
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                PendingIntent.FLAG_IMMUTABLE
+            }else{
+                0
+            }
+        )
 
         val mNotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         //menambahkan berbagai macam elemen pada notifikasi
         val mBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
-            .setSmallIcon(R.drawable.ic_baseline_notifications_24)      //ikon tang muncul pada status bar (wajib)
+            .setContentIntent(pendingIntent)                                //intent ke sebuah activity, url, atau activity lain
+            .setSmallIcon(R.drawable.ic_baseline_notifications_24)          //ikon tang muncul pada status bar (wajib)
             .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.ic_baseline_notifications_24)) // Ikon ini yang akan muncul di sebelah kiri dari text notifikasi.
             .setContentText(resources.getString(R.string.content_text))     // Judul dari notifikasi (wajib ada).
             .setContentTitle(resources.getString(R.string.content_title))   // Text yang akan muncul di bawah judul notifikasi (wajib ada).
