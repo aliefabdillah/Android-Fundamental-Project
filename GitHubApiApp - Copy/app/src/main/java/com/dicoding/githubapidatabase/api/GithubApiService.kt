814726e9f6e3af1bad92028d1596d1885e6307bc
aliefabdillah <aliefmabdillah09@gmail.com>
2022-10-03 20:51:40 +0700

    Commit - delete api key from gradle

--- GitHubApiApp - Copy/app/src/main/java/com/dicoding/githubapidatabase/api/GithubApiService.kt
+++ GitHubApiApp - Copy/app/src/main/java/com/dicoding/githubapidatabase/api/GithubApiService.kt
@@ -1,5 +1,6 @@
 package com.dicoding.githubapidatabase.api
 
+import android.provider.Contacts.SettingsColumns.KEY
 import com.dicoding.githubapidatabase.BuildConfig
 import retrofit2.Call
 import retrofit2.http.GET
@@ -9,19 +10,19 @@ import retrofit2.http.Query
 
 interface GithubApiService {
     // mengambil data saat tombol search ditekan
-    @Headers("Authorization: token ${BuildConfig.KEY}")
+//    @Headers("Authorization: token ${BuildConfig.KEY}")
     @GET("search/users")
     fun getSearchData(
         @Query("q") login : String
     ): Call <GithubResponse>
 
-    @Headers("Authorization: token ${BuildConfig.KEY}")
+//    @Headers("Authorization: token ${BuildConfig.KEY}")
     @GET("users/{login}")
     fun getDetailsData(
         @Path("login") login: String
     ): Call<UsersDetailsResponse>
 
-    @Headers("Authorization: token ${BuildConfig.KEY}")
+//    @Headers("Authorization: token ${BuildConfig.KEY}")
     @GET("users/{login}/{folls}")
     fun getFollowersData(
         @Path("login") login: String,
