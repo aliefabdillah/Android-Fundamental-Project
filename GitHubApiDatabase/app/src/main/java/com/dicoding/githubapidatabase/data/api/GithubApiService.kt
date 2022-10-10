1e0982056bc6841b6fc31c488eeecd06152cb677
aliefabdillah <aliefmabdillah09@gmail.com>
2022-10-10 12:30:13 +0700

    Commit - Revisi bagian ViewPager dan membuat API Key menjadi lokal

--- GitHubApiDatabase/app/src/main/java/com/dicoding/githubapidatabase/data/api/GithubApiService.kt
+++ GitHubApiDatabase/app/src/main/java/com/dicoding/githubapidatabase/data/api/GithubApiService.kt
@@ -9,19 +9,19 @@ import retrofit2.http.Query
 
 interface GithubApiService {
     // mengambil data saat tombol search ditekan
-    @Headers("Authorization: token ${BuildConfig.ApiKey}")
+    @Headers("Authorization: token ${BuildConfig.API_KEY}")
     @GET("search/users")
     fun getSearchData(
         @Query("q") login : String
     ): Call <GithubResponse>
 
-    @Headers("Authorization: token ${BuildConfig.ApiKey}")
+    @Headers("Authorization: token ${BuildConfig.API_KEY}")
     @GET("users/{login}")
     fun getDetailsData(
         @Path("login") login: String
     ): Call<UsersDetailsResponse>
 
-    @Headers("Authorization: token ${BuildConfig.ApiKey}")
+    @Headers("Authorization: token ${BuildConfig.API_KEY}")
     @GET("users/{login}/{folls}")
     fun getFollowersData(
         @Path("login") login: String,
